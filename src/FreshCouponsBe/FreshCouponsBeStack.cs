9b05e1e97326ed3b6433e7aed5d793d0c717044d
kajan <gmkajan@gmail.com>
2021-10-29 19:57:39 +0530

    Initial commit

---
+++ src/FreshCouponsBe/FreshCouponsBeStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace FreshCouponsBe
+{
+    public class FreshCouponsBeStack : Stack
+    {
+        internal FreshCouponsBeStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
