fde2199855736c973e0bce7d1928d76498434d71
kajan <gmkajan@gmail.com>
2021-10-31 16:26:47 +0530

    feat: collect disk-udemy links

---
+++ FetchAndSaveUdemyCouponsHandler/src/FetchAndSaveUdemyCouponsHandler/Helpers/DiskUdemyHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using AngleSharp;
+using AngleSharp.Html.Dom;
+
+namespace FetchAndSaveUdemyCouponsHandler.Helpers
+{
+    public static class DiskUdemyHelper
+    {
+        public static async Task<List<string>> ParseDiskUdemyLinksAsync(string htmlResponseStr,
+            IBrowsingContext browsingContext = null)
+        {
+            browsingContext ??= BrowsingContext.New(Configuration.Default);
+            var document = await browsingContext.OpenAsync(req => req.Content(htmlResponseStr));
+
+            var diskUdemyLinks =
+                document.QuerySelectorAll(DomSelectors.DiskUdemyCouponLink)
+                    .Select(a => ((IHtmlAnchorElement)a).Href)
+                    .ToList();
+
+            LambdaLogger.Log($"Found {diskUdemyLinks.Count} disk udemy links.");
+
+            return diskUdemyLinks;
+        }
+
+        private static class DomSelectors
+        {
+            public const string DiskUdemyCouponLink = ".card .card-header";
+        }
+    }
+}
---
+++ FetchAndSaveUdemyCouponsHandler/test/FetchAndSaveUdemyCouponsHandler.Tests/DiskUdemyHelperTests.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using FetchAndSaveUdemyCouponsHandler.Helpers;
+using Xunit;
+
+namespace FetchAndSaveUdemyCouponsHandler.Tests
+{
+    public class DiskUdemyHelperTests
+    {
+        [Fact]
+        public async Task ParsingDiskUdemyListingPageShouldSucceed()
+        {
+            #region Arrange
+
+            var testHtml = await File.ReadAllTextAsync("./resources/disk-udemy-listing.html");
+
+            #endregion
+
+            #region Act
+
+            var links = await DiskUdemyHelper.ParseDiskUdemyLinksAsync(testHtml);
+
+            #endregion
+
+            #region Assert
+
+            Assert.Equal(15, links.Count);
+            Assert.All(links, l => l.Contains("https://www.diskudemy.com/"));
+
+            #endregion
+        }
+    }
+}
